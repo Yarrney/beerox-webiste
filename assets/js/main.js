c87aa1e40283f422dcc9eea4e387d8a72ca73193
Yarrney <alex.arney@gmail.com>
2016-07-28 20:59:29 +1000

    Uploaded the files for realz this time

---
+++ assets/js/main.js
@@ -0,0 +1,148 @@
+/*
+	Tessellate by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	skel.breakpoints({
+		wide: '(max-width: 1680px)',
+		normal: '(max-width: 1280px)',
+		narrow: '(max-width: 1000px)',
+		mobile: '(max-width: 736px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Scrolly links.
+			$('.scrolly').scrolly();
+
+		// Prioritize "important" elements on narrow.
+			skel.on('+narrow -narrow', function() {
+				$.prioritize(
+					'.important\\28 narrow\\29',
+					skel.breakpoint('narrow').active
+				);
+			});
+
+	});
+
+
+// FORM VALIDATION
+/*$(document).ready(function () {
+
+    $('#ajax-contact').validate({ // initialize the plugin
+        rules: {
+    	       fname: {
+                required: true
+              },
+            email: {
+                required: true,
+                email: true
+            },
+            message: {
+                required: true,
+                minlength: 5
+            }
+        }
+    });
+
+});
+*/
+
+
+//FORM AJAX REQUEST
+})(jQuery);
+
+$(function() {
+
+	// Get the form.
+	var form = $('#ajax-contact');
+
+	// Get the messages div.
+	var formMessages = $('#form-messages');
+
+	// Form field validation
+	$(form).validate({ // initialize the plugin
+        rules: {
+    	       fname: {
+                required: true
+              },
+            email: {
+                required: true,
+                email: true
+            },
+            message: {
+                required: true,
+                minlength: 5
+            }
+        }
+    });
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#fname').val('');
+			$('#lname').val('')
+			$('#email').val('');
+			$('#message').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+
+	});
+
+});
+
+
+
+
+
